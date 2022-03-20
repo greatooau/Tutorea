01821a40014402a2376ddc5e18fae268dc7f01e8
greatooau <pajaroazulx@gmail.com>
2022-03-20 16:41:20 -0600

    l

--- screens/Login.js
+++ screens/Login.js
@@ -1,24 +1,70 @@
-import { useState } from 'react';
-import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, Image} from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
+//Components
+import AppButton from '../components/AppButton';
 
-const Login = () => {
+export default Login = () => {
     return (
-        <SafeAreaView style={styles.page}>
+        <LinearGradient colors={['rgb(28,50,82)', 'transparent']} style={styles.page}>
 
-            <View style={styles.title}>
-                <Text>Bienvenido a Tutorea</Text>
+            <View style={styles.titleContainer}>
+              <Text style={styles.titleText}>Bienvenido a Tutorea!</Text>
             </View>
-        </SafeAreaView>
+
+            <View style={styles.imageContainer}>
+              <Image style={styles.image} source={require('../assets/img/welcome.png')}/>
+            </View>
+            <View style={styles.buttons}>
+              <AppButton buttonText="Ingresar"/>
+              <AppButton buttonText="Registrar"/>
+            </View>
+
+            <View style={styles.bottomTextContainer}>
+              <Text style={styles.bottomText}>Tutorea</Text>
+            </View>
+            <StatusBar style="auto" />
+
+        </LinearGradient>
     );
-};
+}
+
 
 const styles = StyleSheet.create({
     page: {
-
+      flex:1,
     },
-    title: {
-
+    titleContainer: {
+      flexDirection: 'row',
+      marginTop: 50,
+      justifyContent: 'center',
+      paddingHorizontal:50
+    },
+    titleText: {
+      fontSize: 35,
+      color: '#ffff',
+      textAlign:'center'
+    },
+    imageContainer: {
+      flexDirection: 'row',
+      justifyContent:'center',
+      paddingVertical:65,
+    },
+    image: {
+      maxHeight:170,
+      maxWidth: 350,
+    },
+    buttons: {
+      paddingBottom: 180
+    },
+    bottomTextContainer: {
+      flexDirection: 'row',
+      justifyContent: 'center',
+    },
+    bottomText: {
+      fontFamily: 'Lato',
+      color: '#1c3252',
+      fontSize: 35,
+      fontWeight: '700'
     }
 });
-
-export default Login;
